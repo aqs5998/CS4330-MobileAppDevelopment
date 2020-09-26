fcd1bcdd85a13450346a887589f717c6ba4e0e67
Erik-Mtz <erik.martinez98@outlook.com>
2020-09-25 19:53:35 -0600

    added even listeners

--- app/src/main/java/cs4330/pricewatcher/MainActivity.java
+++ app/src/main/java/cs4330/pricewatcher/MainActivity.java
@@ -3,6 +3,8 @@ package cs4330.pricewatcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -32,5 +34,31 @@ public class MainActivity extends AppCompatActivity {
         itemUpdatePriceButton = findViewById(R.id.ItemUpdatePriceButton);
         visitItemOnlineButton = findViewById(R.id.VisitItemOnlineButton);
 
+
+        // Add on click listeners
+        itemUpdatePriceButton.setOnClickListener(this::itemUpdatePriceButtonClicked);
+        visitItemOnlineButton.setOnClickListener(this::visitItemOnlineButtonClicked);
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("Main", "onStart() -------------- ");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("Main", "OnResume()-------------- ");
+    }
+
+    public void itemUpdatePriceButtonClicked(View view){
+        Log.d("Main", "itemUpdatePriceButtonClicked()-------------- ");
+    }
+
+    public void visitItemOnlineButtonClicked(View view){
+        Log.d("Main", "visitItemOnlineButtonClicked()-------------- ");
+    }
+
+
 }
