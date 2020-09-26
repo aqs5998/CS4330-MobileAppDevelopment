785de17ab87f6419d2369de60f42572e320a4ef3
Erik-Mtz <erik.martinez98@outlook.com>
2020-09-25 19:49:18 -0600

    updated buttons and textviews

--- app/src/main/java/cs4330/pricewatcher/MainActivity.java
+++ app/src/main/java/cs4330/pricewatcher/MainActivity.java
@@ -4,17 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    PriceWatcher pricewatcher;
+    private PriceWatcher pricewatcher;
+    private TextView itemNameTextView;
+    private TextView itemInitialPriceTextView;
+    private TextView itemCurrentPriceTextView;
+    private TextView itemPercentChangeTextView;
+    private Button itemUpdatePriceButton;
+    private Button visitItemOnlineButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // This current pricewatcher instance only has one method and is for placeholder purposes
         pricewatcher = new PriceWatcher();
 
-        Button updatePriceButton = getCon
+        // Initialize all TextViews and Buttons
+        itemNameTextView = findViewById(R.id.ItemNameTextView);
+        itemInitialPriceTextView = findViewById(R.id.ItemInitialPriceTextView);
+        itemCurrentPriceTextView = findViewById(R.id.ItemCurrentPriceTextView);
+        itemPercentChangeTextView = findViewById(R.id.ItemPercentChangeTextView);
+        itemUpdatePriceButton = findViewById(R.id.ItemUpdatePriceButton);
+        visitItemOnlineButton = findViewById(R.id.VisitItemOnlineButton);
+
     }
 }
